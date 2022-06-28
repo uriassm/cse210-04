eca8c290e5d9dd35da7c6b11035abae5bd496fad
uriassm <uriassmira@gmail.com>
2022-06-27 17:37:31 -0700

    Submit working version of Greed game.

--- README.md
+++
@@ -1,2 +0,0 @@
-# cse210-04
-Greed game project. 
---
+++ greed/greed/game/casting/artifact.py
@@ -0,0 +1,33 @@
+from game.casting.actor import Actor
+from game.shared.point import Point
+
+
+class Artifact(Actor):
+    """
+    An item of cultural or historical interest. 
+    
+    The responsibility of an Artifact is to provide a message about itself.
+
+    Attributes:
+        _message (string): A short description about the artifact.
+    """
+    def __init__(self):
+        super().__init__()
+        self._message = ""
+        self._velocity = Point(0, 3)
+        
+    def get_message(self):
+        """Gets the artifact's message.
+        
+        Returns:
+            string: The message.
+        """
+        return self._message
+    
+    def set_message(self, message):
+        """Updates the message to the given one.
+        
+        Args:
+            message (string): The given message.
+        """
+        self._message = message
